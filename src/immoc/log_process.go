b4d550097824b70cc1679e6ecaf6c1b8e8135fd8
zlcting <zlcting@gmail.com>
2018-06-05 17:31:34 +0800

    日志系统

--- src/immoc/log_process.go
+++ src/immoc/log_process.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 	"strings"
 	"time"
 )
 
 type Reader interface {
-	Read(rc chan string)
+	Read(rc chan []byte)
 }
 
 type Writer interface {
@@ -15,7 +18,7 @@ type Writer interface {
 }
 
 type LogProcess struct {
-	rc          chan string
+	rc          chan []byte
 	wc          chan string
 	read        Reader
 	write       Writer
@@ -30,32 +33,55 @@ type WriteToInfluxDB struct {
 	influxDBDsn string
 }
 
-func (r *ReadFromFile) Read(rc chan string) {
+func (r *ReadFromFile) Read(rc chan []byte) {
 	//读取模块
-	line := "message"
-	rc <- line
+	//打开文件
+	f, err := os.Open(r.path)
+	if err != nil {
+		panic(fmt.Sprintf("open file error %s", err.Error()))
+	}
+
+	//从文件的末尾开始逐行读取文件内容
+	f.Seek(0, 2)
+	rd := bufio.NewReader(f)
+	for {
+		line, err := rd.ReadBytes('\n')
+		if err == io.EOF {
+			time.Sleep(500 * time.Millisecond)
+			continue
+		} else if err != nil {
+			panic(fmt.Sprintf("readbytes error :s", err.Error()))
+		}
+		rc <- line[:len(line)-1]
+	}
+
 }
 
 func (w *WriteToInfluxDB) Write(wc chan string) {
-	fmt.Println(<-wc)
+
+	for v := range wc {
+		fmt.Println(v)
+	}
+
 }
 
 func (l *LogProcess) Process() {
 	//解析模块
-	data := <-l.rc
-	l.wc <- strings.ToUpper(data)
+	for v := range l.rc {
+		l.wc <- strings.ToUpper(string(v))
+	}
 }
 
 func main() {
 	r := &ReadFromFile{
-		path: "tmp/asd.txt",
+		path: "./access.log",
 	}
 	w := &WriteToInfluxDB{
 		influxDBDsn: "username",
 	}
 
 	lp := &LogProcess{
-		rc:    make(chan string),
+		rc:    make(chan []byte),
 		wc:    make(chan string),
 		read:  r,
 		write: w,
@@ -65,5 +91,5 @@ func main() {
 	go lp.Process()
 	go lp.write.Write(lp.wc)
 
-	time.Sleep(1 * time.Second)
+	time.Sleep(30 * time.Second)
 }
